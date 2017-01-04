f41de117dde07e11aef06f7a2f6013f090670683
Emil Thorndal <Emilthorndal3@Gmail.com>
2017-01-04 13:31:11 +0100

    0.0.1
    Main Menu v0.1

---
+++ Assets/WhenClicked.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WhenClicked : MonoBehaviour {
+
+	public Button StartGame;
+
+	void Start () {
+		Button btn = StartGame.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		Debug.Log ("You have clicked the button!");
+	}
+
+}
